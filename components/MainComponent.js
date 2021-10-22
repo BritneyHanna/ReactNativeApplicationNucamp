767c6016ddc08b9b0c5e1da26a7f713593380b2c
unknown <b.ahanna@yahoo.com>
2021-10-22 19:33:29 -0400

    Week4 assignment attempt

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -326,27 +326,37 @@ const AppNavigator = createAppContainer(MainNavigator)
 class Main extends Component {
 
     componentDidMount() {
+
+
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        this.showNetInfo();
     }
 
     componentWillUnmount() {
         this.unsubscribeNetInfo();
     }
     
+    showNetInfo=async()=>{
+        
+
+       const connectionInfo =   await NetInfo.fetch();
+            (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+     
+
+        // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        //     this.handleConnectivityChange(connectionInfo);
+        // });
+
+    }
+
+
     handleConnectivityChange = connectionInfo => {
         let connectionMsg = 'You are now connected to an active network.';
         switch (connectionInfo.type) {
